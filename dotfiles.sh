7129ef5f2997384eed7143c32c9576567b2a8c88
Alan.Shan <apple.shan@gmail.com>
2018-04-16 17:40:25 +0800

    i manage symlinks with stow

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,128 +1,51 @@
 #!/usr/bin/env bash
-# NOTE:
-#     TO GET RID OF THE VERBOSE OUTPUT, REDIRECT STDOUT TO /dev/null
-#TODO: handle changing item conf from l to c
-#TODO: error whene sync /etc without root permission
 
-dotfile () {
-    if [ ! -f "dotfiles-${2}.list" ]; then
-        echo "ERROR: configuration file 'dotfiles-${2}.list' not found" >&2
-        exit 1
-    fi
-    if [ ! -d "$SYNC_DIR" ];then
-        echo "ERROR: ${SYNC_DIR} dir to sync not found on $(pwd)" >&2
-        exit 1
-    fi
-    echo "$1" "$2":
-    grep "^\w*[lc] \+[^[:space:]]\+\w*$" dotfiles-"${2}".list | \
-        while read -r line; do
-            ITEM_TO_SYNC=$(awk '{print $2}' <<< "$line")
+# dotfiles utility - https://gist.github.com/Jamesits/9bc4adfb1f299380c79e
+# Set $DOTFILES to where you want to put your dotfiles.
+# then run dotfiles-init someSoftware,
+# and it will move all files starting with `.someSoftware` to the correct location
+# then link them back,
+# Which will produce a directory structure like:
+# 
+# $ tree -aL 2 ~/projects/private/dotfiles
+# ~/projects/private/dotfiles :
+# ├── dotfiles.sh
+# └── shell
+#     └── .zshrc
+#
 
-            if [ ! -e "$SYNC_DIR/$ITEM_TO_SYNC" ]; then
-                echo "ERROR: item not found @ $SYNC_DIR/$ITEM_TO_SYNC'" >&2
-                continue
-            fi
-            $1 "$(awk '{print $1}' <<< "$line")"
-        done
+# Where to store your actual config files
+export DOTFILES=~/projects/private/dotfiles
+
+# List unlinked dotfiles
+dotfiles-count() {
+        pushd >/dev/null 2>&1
+        cd $HOME
+        ls -ald .* | grep -v ^l | tee >(wc -l)
+        popd >/dev/null 2>&1
 }
 
-sync () {
-    if [[ "$1" == "c"  && -e $TARGET_DIR/$ITEM_TO_SYNC ]]; then
-        diff {"$SYNC_DIR","$TARGET_DIR"}/"$ITEM_TO_SYNC" > /dev/null \
-            && echo "DID: $ITEM_TO_SYNC" \
-            || echo "WARN: different item '$TARGET_DIR/$ITEM_TO_SYNC' exists" >&2
-        return
-    fi
-    if [[ "$1" == "l" && -e $TARGET_DIR/$ITEM_TO_SYNC ]];then
-        if [[ $(stat -L -c '%i' "$TARGET_DIR/$ITEM_TO_SYNC") == \
-            $(stat -L -c '%i' "$SYNC_DIR/$ITEM_TO_SYNC") ]]; then
-            echo "DID: $ITEM_TO_SYNC"
-            return
-        fi
-        echo "WARN: different item '$TARGET_DIR/$ITEM_TO_SYNC' exists" >&2
-        return
-    fi
-    mkdir -p $(dirname "$TARGET_DIR/$ITEM_TO_SYNC")
-    case $1 in
-        l) ln -s "$SYNC_DIR/$ITEM_TO_SYNC" "$TARGET_DIR/$ITEM_TO_SYNC"
-           echo "LINKED: $ITEM_TO_SYNC"
-           ;;
-        c) cp -r "$SYNC_DIR/$ITEM_TO_SYNC" "$TARGET_DIR/$ITEM_TO_SYNC"
-           echo "COPYED: $ITEM_TO_SYNC"
-           ;;
-    esac
+# Move and link files named `.something*`
+dotfiles-init() {
+        pushd >/dev/null 2>&1
+        cd $HOME
+        ls -ald .$1*;
+        mkdir -p $DOTFILES/$1;
+        mv .$1* $DOTFILES/$1;
+        stow --dir=$DOTFILES --target=$HOME -vv $1
+        popd >/dev/null 2>&1
 }
 
-status () {
-    if [[ ! -e $TARGET_DIR/$ITEM_TO_SYNC ]]; then
-        echo "NOSYNC: $ITEM_TO_SYNC" >&2
-        return
-    fi
-    case $1 in
-        l)
-            if [[ $(stat -L -c '%i' "$TARGET_DIR/$ITEM_TO_SYNC") == \
-                $(stat -L -c '%i' "$SYNC_DIR/$ITEM_TO_SYNC") ]]; then
-                echo "MATCH : $ITEM_TO_SYNC"
-            else
-                echo "DIFFER: $ITEM_TO_SYNC" >&2
-            fi
-            ;;
-        c)
-            diff {"$SYNC_DIR","$TARGET_DIR"}/"$ITEM_TO_SYNC" > /dev/null \
-                && echo "MATCH : $ITEM_TO_SYNC" \
-                || echo "DIFFER: $ITEM_TO_SYNC" >&2
-            ;;
-    esac
+# rebuild links - useful when you are recovering settings to a new OS
+# run `dotfiles-rebuild *` to rebuild all at once
+dotfiles-rebuild() {
+        stow --dir=$DOTFILES --target=$HOME -vv $@
 }
 
-help () {
-    cat << EOF
-$(basename "$0"): sync dotfiles
-USAGE: $(basename "$0") COMMAND PROFILE
-command:
-        sync: sync dotfiles of profile
-        status: check diff/status of profile's dotfiles
-profile:
-        home: dotfiles of $HOME config at dotfiles-home.list
-        etc: dotfiles of /etc config at dotfiles-etc.list
-        opt: dotfiles of /opt config at dotfiles-opt.list
-EOF
-}
+# dotfiles-rebuild() :
+# $ stow -d $HOME/projects/private/dotfiles -t $HOME -vv shell
 
-CWD=$(readlink -f "$(dirname "$0")")
+# usage
+# $ dotfiles-rebuild shell
 
-if [ $# -ne 2 ]; then
-    # echo "ERROR: unvalid numbber of args" >&2
-    help >&2
-    exit 1
-fi
-
-case $2 in
-    etc)
-        SYNC_DIR="$CWD/etc"
-        TARGET_DIR="/etc"
-        ;;
-    home)
-        SYNC_DIR="$CWD/user-home"
-        # TARGET_DIR="$HOME"
-        TARGET_DIR="/home/appleshan"
-        ;;
-    opt)
-        SYNC_DIR="$CWD/opt"
-        TARGET_DIR="/opt"
-        ;;
-    *)
-        echo "ERROR: unsupported profile" >&2
-        help >&2
-        exit 1
-        ;;
-esac
-
-case $1 in
-    sync|status)
-        dotfile "$1" "$2";;
-    *)
-        echo "ERROR: unsupported command" >&2
-        help >&2
-        exit 1;;
-esac
+# $ ls -alF | grep "^l"
