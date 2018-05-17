20fc7307065b20923d049c86cfc158f5ae942b7c
Alan.Shan <apple.shan@gmail.com>
2018-05-17 11:28:09 +0800

    add i3lock-fancy-multimonitor

--- rofi/.config/rofi/powermenu.sh
+++ rofi/.config/rofi/powermenu.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+# rofi_power menu
+
 action=$(echo -e "lock\nlogout\nshutdown\nreboot" | rofi -dmenu -p "power:")
 
 if [[ "$action" == "lock" ]]
 then
-    ~/.i3/i3lock-fancy-multimonitor/lock
+    ~/.config/i3lock-fancy-multimonitor/lock
 fi
 
 if [[ "$action" == "logout" ]]
