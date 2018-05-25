d8ce7b50a5ffabc153c9fea7f40073108c29063d
appleshan <apple.shan@gmail.com>
2018-05-25 19:07:37 +0800

    set polybar config

--- i3wm/.config/polybar/scripts/info-redshift-temp.sh
+++ i3wm/.config/polybar/scripts/info-redshift-temp.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 if [ "$(pgrep -x redshift)" ]; then
-    temp=$(redshift -p 2> /dev/null | grep temp | cut -d ":" -f 2 | tr -dc "[:digit:]")
+    temp=$(redshift -p 2> /dev/null \
+                    | grep "色温" \
+                    | cut -d ":" -f 2 \
+                    | tr -dc "[:digit:]")
 
     if [ -z "$temp" ]; then
         echo "%{F#65737E} #"
