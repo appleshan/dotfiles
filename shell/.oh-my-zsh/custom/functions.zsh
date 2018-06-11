6478e06808603bf1fe61a005bf0558f2342abe0b
appleshan <apple.shan@gmail.com>
2018-06-11 17:38:28 +0800

    modify confit of urxvt

--- shell/.oh-my-zsh/custom/functions.zsh
+++ shell/.oh-my-zsh/custom/functions.zsh
@@ -33,8 +33,8 @@ export PROMPT_COMMAND="history -a; history -n; ${PROMPT_COMMAND}"   # mem/file s
 # Disable options:
 unset MAILCHECK       # I don not want my shell to warn me of incoming mail
 
-export TERM=rxvt
-export TERMINAL=rxvt
+export TERM=rxvt-unicode
+export TERMINAL=rxvt-unicode
 
 source ~/.local/bin/ssh-connect/ssh-connect.sh
 
