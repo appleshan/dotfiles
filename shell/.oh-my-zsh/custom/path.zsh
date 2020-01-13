702c3ce1ba7679049bc09e946ce2cfa8b8cf5f3f
appleshan <apple.shan@gmail.com>
2020-01-14 00:02:10 +0800

    Add: pyenv 添加一个变量

--- shell/.oh-my-zsh/custom/path.zsh
+++ shell/.oh-my-zsh/custom/path.zsh
@@ -113,6 +113,7 @@ export PYTHONDONTWRITEBYTECODE=1
 # Pyenv
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
+export PYENV_VIRTUALENV_DISABLE_PROMPT=1
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 
