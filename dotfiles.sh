d7b4622316896a984b8c3cc73ed320cc85d0a111
appleshan <apple.shan@gmail.com>
2020-05-13 09:53:52 +0800

    Clean: dotfiles.sh 中的缩进调整为4个空格

--- dotfiles.sh
+++ dotfiles.sh
@@ -19,27 +19,27 @@ export DOTFILES=~/projects-private/dotfiles
 
 # List unlinked dotfiles
 dotfiles-count() {
-        pushd >/dev/null 2>&1
-        cd $HOME
-        ls -ald .* | grep -v ^l | tee >(wc -l)
-        popd >/dev/null 2>&1
+    pushd >/dev/null 2>&1
+    cd $HOME
+    ls -ald .* | grep -v ^l | tee >(wc -l)
+    popd >/dev/null 2>&1
 }
 
 # Move and link files named `.something*`
 dotfiles-init() {
-        pushd >/dev/null 2>&1
-        cd $HOME
-        ls -ald .$1*;
-        mkdir -p $DOTFILES/$1;
-        mv .$1* $DOTFILES/$1;
-        stow --dir=$DOTFILES --target=$HOME -vv $1
-        popd >/dev/null 2>&1
+    pushd >/dev/null 2>&1
+    cd $HOME
+    ls -ald .$1*;
+    mkdir -p $DOTFILES/$1;
+    mv .$1* $DOTFILES/$1;
+    stow --dir=$DOTFILES --target=$HOME -vv $1
+    popd >/dev/null 2>&1
 }
 
 # rebuild links - useful when you are recovering settings to a new OS
 # run `dotfiles-rebuild *` to rebuild all at once
 dotfiles-rebuild() {
-        stow --dir=$DOTFILES --target=$HOME -vv $@
+    stow --dir=$DOTFILES --target=$HOME -vv $@
 }
 
 # delete
