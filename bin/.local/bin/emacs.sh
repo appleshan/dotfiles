a46b6d243e24623366dee4d98acb4f122dfcba18
Alan.Shan <apple.shan@gmail.com>
2020-11-06 01:32:08 +0800

    Docs: emacs.sh 里引用 csslayer 的文章

--- bin/.local/bin/emacs.sh
+++ bin/.local/bin/emacs.sh
@@ -3,5 +3,6 @@
 
 # 在 Emacs 中使用 fcitx 输入法
 # @See https://bbs.archlinuxcn.org/viewtopic.php?pid=15534#p15534
+# @See https://www.csslayer.info/wordpress/
 env LC_CTYPE="zh_CN.UTF-8" /usr/bin/emacs "$@"
 
