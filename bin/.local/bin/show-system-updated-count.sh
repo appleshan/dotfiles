5778f8e9e1d362e022420cbe84c84614747aea7f
Alan.Shan <apple.shan@gmail.com>
2020-11-06 11:01:04 +0800

    Fix: bash中的 $表达式用引号包裹

--- bin/.local/bin/show-system-updated-count.sh
+++ bin/.local/bin/show-system-updated-count.sh
@@ -2,8 +2,8 @@
 
 # Query the update times of archlinux system
 # 查询archlinux滚了多少次
-echo $(head -n1 /var/log/pacman.log | cut -d " " -f 1,2) \
+echo "$(head -n1 /var/log/pacman.log | cut -d " " -f 1,2)" \
      以来一共滚了 \
-     $(grep -c "full system upgrade" /var/log/pacman.log) \
+     "$(grep -c 'full system upgrade' /var/log/pacman.log)" \
      次
 
