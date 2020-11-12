8e21a075610bf4448516ab9002bcb2fe46be2181
Alan.Shan <apple.shan@gmail.com>
2020-11-12 14:22:44 +0800

    Feature: 显示终端色彩表, 排列更加有序

--- bin/.local/bin/term-colors.sh
+++ bin/.local/bin/term-colors.sh
@@ -1,33 +1,17 @@
 #!/bin/bash
 # 显示终端色彩表
 
-mod=$1
-color () {
-  printf "\e[01;$mod;38;5;%sm%4s" $1 $1;
-}
+for fgbg in 38 48 ; do # Foreground / Background
+    for color in {0..255} ; do # Colors
+        # Display the color
+        printf "\e[${fgbg};5;%sm  %3s  \e[0m" $color $color
+        # Display 6 colors per lines
+        if [ $((($color + 1) % 6)) == 4 ] ; then
+            echo # New line
+        fi
+    done
+    echo # New line
+done
 
-# 基本 16 色
-for i in {0..7}; do
-  color $i
-done
-echo
-for i in {8..15}; do
-  color $i
-done
-echo;echo
-
-for k in `seq 0 1`;do 
-  for j in `seq $((16+k*18)) 36 $((196+k*18))`;do 
-    for i in `seq $j $((j+17))`; do 
-      printf "\e[01;$1;38;5;%sm%4s" $i $i;
-    done;echo;
-  done;
-done
-echo
-
-# 灰色
-for i in {232..255}; do
-  color $i
-done
-echo
+exit 0
 
