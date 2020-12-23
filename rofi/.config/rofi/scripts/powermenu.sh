53ec7b59aee5e1a27436f53384c74b29e33f28c8
Alan.Shan <apple.shan@gmail.com>
2020-12-23 23:56:31 +0800

    Clean: rofi: 整理关机菜单，移除没有用的功能

--- rofi/.config/rofi/scripts/powermenu.sh
+++ rofi/.config/rofi/scripts/powermenu.sh
@@ -4,34 +4,26 @@
 option0=" lock"
 option1=" logout"
 option2=" suspend"
-option3=" scheduled suspend (10min)"
-option4=" scheduled suspend (20min)"
-option5=" scheduled suspend (30min)"
-option6=" reboot"
-option7=" shutdown"
+option3=" reboot"
+option4=" shutdown"
 
 # options passed into variable
-options="$option0\n$option1\n$option2\n$option3\n$option4\n$option5\n$option6\n$option7"
+options="$option0\n$option1\n$option2\n$option3\n$option4"
 
-chosen="$(echo -e "$options" | rofi -lines 8 -width -26 -dmenu -p "power")"
+#chosen="$(echo -e "$options" | rofi -lines 8 -width -22 -dmenu -p "power")"
+chosen="$(echo -e "$options" | rofi -lines 8 -columns 12 -dmenu -p "power")"
 case $chosen in
     $option0)
         # i3lock-fancy;;
         $HOME/.config/i3lock-fancy-multimonitor/lock;;
     $option1)
         # i3-msg exit;;
-i3-nagbar -t warning -m 'You pressed the exit shortcut. Do you really want to exit i3? This will end your X session.' -b 'Yes, exit i3' 'i3-msg exit';;
+        i3-nagbar -t warning -m 'You pressed the exit shortcut. Do you really want to exit i3? This will end your X session.' -b 'Yes, exit i3' 'i3-msg exit';;
     $option2)
         systemctl suspend;;
-	$option3)
-		sleep 600 && systemctl suspend;;
-	$option4)
-		sleep 1200 && systemctl suspend;;
-	$option5)
-		sleep 1800 && systemctl suspend;;
-    $option6)
+    $option3)
         systemctl reboot;;
-	$option7)
+	$option4)
         systemctl poweroff;;
 esac
 
