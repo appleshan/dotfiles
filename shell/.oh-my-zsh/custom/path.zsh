ab9281455d3081af681936b081af5dd9e2f01a30
Alan.Shan <apple.shan@gmail.com>
2021-01-23 22:14:52 +0800

    Fix: 更新 nvm 的初始化配置

--- shell/.oh-my-zsh/custom/path.zsh
+++ shell/.oh-my-zsh/custom/path.zsh
@@ -134,9 +134,7 @@ appendpath "$PIPSI_HOME/bin"
 # Node.js       #
 #################
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+source /usr/share/nvm/init-nvm.sh
 
 #################
 # dotnet-sdk    #
