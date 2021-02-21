b7380a2ae46afbb9adac5d76784fc679deaabda3
Alan.Shan <apple.shan@gmail.com>
2021-02-21 15:03:17 +0800

    Fix: 显示机场客户端的开启标志

---
+++ polybar/.config/polybar/scripts/isrunning-proxy-client.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ "$(pgrep -x shadowsocks2)" ]; then
+    echo ""
+elif [ "$(pgrep -x ss-qt5)" ]; then
+    echo ""
+elif [ "$(pgrep -x Trojan-Qt5)" ]; then
+    echo ""
+elif [ "$(pgrep -x qv2ray)" ]; then
+    echo ""
+else
+    echo ""
+fi
+
