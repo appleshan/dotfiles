5ddec8940445aa61de8c32a6747f2be9baeefd16
Alan.Shan <apple.shan@gmail.com>
2021-07-16 21:00:23 +0800

    Fix: CPU 风扇速度的数值去掉数量级 k

--- polybar/.config/polybar/scripts/system-fan-speed/system-fan-speed.sh
+++ polybar/.config/polybar/scripts/system-fan-speed/system-fan-speed.sh
@@ -4,8 +4,8 @@
 speed=$(sensors | grep fan2 | cut -d " " -f 19)
 
 if [ "$speed" != "" ]; then
-    speed_round=$(echo "scale=1;$speed/1000" | bc -l )
-    echo "$speed_round kRPM"
+    speed_round=$(echo "scale=1;$speed" | bc -l )
+    echo "$speed_round RPM"
 else
     echo "0 RPM"
 fi
