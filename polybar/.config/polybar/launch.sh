f35390fa178fe3012a3e2d71d9f9d2273e7f2d9a
appleshan <apple.shan@gmail.com>
2021-07-31 13:39:57 +0800

    Feature: polybar launch

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,26 +6,35 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-# while pgrep -x polybar >/dev/null; do sleep 1; done
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
-# Launch top and bottom
-# polybar top &
-# polybar bottom &
+# Detect if secondary monitor is connected, if so, add a specific bar and move tray to it
+# Else, keep tray on main monitor
+# see https://github.com/polybar/polybar/issues/763
 
-# Stolen from: https://github.com/jaagr/polybar/issues/763
-if type "xrandr"; then
-    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-        if [ "$m" = "DP1" ]; then
-            MONITOR=$m polybar -l=error -c ~/.config/polybar/config.ini top &
-            MONITOR=$m polybar -l=error -c ~/.config/polybar/config.ini bottom &
-        else
-            MONITOR=$m polybar -l=error -c ~/.config/polybar/config.ini bottom &
-        fi
-    done
-else
-    polybar -l=error -c ~/.config/polybar/config.ini top &
-    polybar -l=error -c ~/.config/polybar/config.ini bottom &
-fi
+outputs=$(xrandr --query | grep " connected" | cut -d" " -f1)
+set -- $outputs
+tray_output=$1
+
+for m in $outputs; do
+    if [ $m == $1 ]; then
+        MONITOR1=$m polybar -l=error -c ~/.config/polybar/config.ini top &
+        MONITOR1=$m polybar -l=error -c ~/.config/polybar/config.ini bottom &
+    elif [ $m == $2 ]; then
+        tray_output=$m
+        MONITOR2=$m polybar -l=error -c ~/.config/polybar/config.ini bottom &
+    else
+        MONITOR1=$m polybar -l=error -c ~/.config/polybar/config.ini bottom &
+    fi
+done
+
+for m in $outputs; do
+    export MONITOR1=$1
+    export MONITOR2=$2
+    export TRAY_POSITION=none
+    if [[ $m == $tray_output ]]; then
+        TRAY_POSITION=right
+    fi
+done
 
 echo "Bars launched..."
