5784e7c945dd2143f50a2d5aca8a7d24a8e63422
Alan.Shan <apple.shan@gmail.com>
2021-08-09 14:23:14 +0800

    Add: zsh plugin: history-search-multi-word

--- shell/.oh-my-zsh/custom/zsh-theme.zsh
+++ shell/.oh-my-zsh/custom/zsh-theme.zsh
@@ -45,3 +45,12 @@ POWERLEVEL9K_VCS_BRANCH_ICON=$'\uF126 '
 
 # powerlevel10k
 ZSH_THEME="powerlevel10k/powerlevel10k"
+
+# {{ history-search-multi-word
+zstyle ":history-search-multi-word" page-size "20"                     # Number of entries to show (default is $LINES/3)
+zstyle ":history-search-multi-word" highlight-color "fg=yellow,bold"   # Color in which to highlight matched, searched text (default bg=17 on 256-color terminals)
+zstyle ":plugin:history-search-multi-word" synhl "yes"                 # Whether to perform syntax highlighting (default true)
+zstyle ":plugin:history-search-multi-word" active "underline"          # Effect on active history entry. Try: standout, bold, bg=blue (default underline)
+zstyle ":plugin:history-search-multi-word" check-paths "yes"           # Whether to check paths for existence and mark with magenta (default true)
+zstyle ":plugin:history-search-multi-word" clear-on-cancel "no"        # Whether pressing Ctrl-C or ESC should clear entered query
+# }}
