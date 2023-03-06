5f0f98cc28cf8b80aa7afc99dadc3c7f9a11e60d
appleshan <apple.shan@gmail.com>
2023-03-06 16:37:05 +0800

    更新 polybar 的 modules

--- polybar/.config/polybar/scripts/popup-calendar/popup-calendar.sh
+++ polybar/.config/polybar/scripts/popup-calendar/popup-calendar.sh
@@ -5,7 +5,7 @@ BORDER_SIZE=1  # border size from your wm settings
 YAD_WIDTH=222  # 222 is minimum possible value
 YAD_HEIGHT=193 # 193 is minimum possible value
 # DATE="$(date +"%a %d %H:%M")"
-DATE="$(date +" %Y-%m-%d  %H:%M")"
+DATE="$(date +"📅 %Y-%m-%d 🕒 %H:%M")"
 
 case "$1" in
 --popup)
