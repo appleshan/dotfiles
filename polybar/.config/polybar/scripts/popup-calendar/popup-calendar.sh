0951b370d64c3157ab6256cadd2ebe7f0d3fcf89
Alan.Shan <apple.shan@gmail.com>
2023-03-11 21:59:35 +0800

    polybar 把 date 替换为 popup-calendar

--- polybar/.config/polybar/scripts/popup-calendar/popup-calendar.sh
+++ polybar/.config/polybar/scripts/popup-calendar/popup-calendar.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-BAR_HEIGHT=22  # polybar height
+BAR_HEIGHT=70  # polybar height
 BORDER_SIZE=1  # border size from your wm settings
 YAD_WIDTH=222  # 222 is minimum possible value
 YAD_HEIGHT=193 # 193 is minimum possible value
-# DATE="$(date +"%a %d %H:%M")"
-DATE="$(date +"📅 %Y-%m-%d 🕒 %H:%M")"
+DATE="$(date +"%Y-%m-%d %H:%M")"
 
 case "$1" in
 --popup)
