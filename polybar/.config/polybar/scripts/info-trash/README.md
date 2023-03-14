e4e4fd82833ad97479eca579f253433b3a9925e8
Alan.Shan <apple.shan@gmail.com>
2023-03-14 18:27:20 +0800

    polybar 更新 info-trash

--- polybar/.config/polybar/scripts/info-trash/README.md
+++ polybar/.config/polybar/scripts/info-trash/README.md
@@ -1,16 +1,8 @@
-# Script: info-trash
+# Polybar-Trash-module
+Module trash for Polybar
 
-This script count the files in you trash directory.
+![Screenshot from 2022-04-27 16-05-31](https://user-images.githubusercontent.com/103053714/165590944-1950ce37-c917-470f-b251-c7777ae5efb4.png)
+![Screenshot from 2022-04-27 18-57-55](https://user-images.githubusercontent.com/103053714/165590957-f12436cb-721c-4ee6-9ec5-854eb82bb34f.png)
+![Screenshot from 2022-04-27 18-59-54](https://user-images.githubusercontent.com/103053714/165590960-9f6a261a-ced5-46d5-a500-b5dc6bb93ae9.png)
 
-![info-trash](screenshots/1.png)
-
-
-## Module
-
-```ini
-[module/info-trash]
-type = custom/script
-exec = ~/polybar-scripts/info-trash.sh
-interval = 60
-click-left = ~/polybar-scripts/info-trash.sh --clean &
-```
+Trash info EMPTY - FULL - (VERY FULL from 5GB) - Autocleaning > 8GB / click-left: open trash
--- polybar/.config/polybar/scripts/info-trash/info-trash.sh
+++ polybar/.config/polybar/scripts/info-trash/info-trash.sh
@@ -1,16 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
-case "$1" in
-    --clean)
-        rm -rf ~/.local/share/Trash/files
-        rm -rf ~/.local/share/Trash/info
-        mkdir ~/.local/share/Trash/files
-        mkdir ~/.local/share/Trash/info
-        ;;
-    *)
-        find ~/.local/share/Trash/files -maxdepth 1 \
-            | grep -v "files$" \
-            | wc -l
-        ;;
-esac
+get_trash() {
+    CHECK=$(du -a $HOME/.local/share/Trash/files/ | wc -l );
+    TRASH=$(expr "$CHECK" - 1);
+    SIZE=$(du -sh $HOME/.local/share/Trash/files | awk '{ printf $1}');
+    EFFECTIVE_SIZE=$(du -s $HOME/.local/share/Trash/files | awk '{ printf $1}');
+    ALLERT=$(printf 5000000);
+}
 
+get_trash
+
+
+if (("$EFFECTIVE_SIZE" > "$ALLERT" )); then
+    echo "%{F#ff5770} %{F#f2f2f2}Very Full %{F#77f2f2}$SIZE""B"
+elif (("$TRASH" > 0)); then
+    echo "%{F#FF8B42} %{F#f2f2f2}Full %{F#77f2f2}$SIZE""B"
+else
+    echo " %{F#44f2f2} %{F#77f2f2}Empty"
+fi
+
+clean_trash(){
+    TRASHSIZE=$( du -s ~/.local/share/Trash/files/ | awk '{ printf $1}' );
+    LIMIT_TRASH=$(printf 8000000);
+}
+
+clean_trash
+if (("$TRASHSIZE" > "$LIMIT_TRASH")); then
+    rm -rf $HOME/.local/share/Trash/files/*
+fi
