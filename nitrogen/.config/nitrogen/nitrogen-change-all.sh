908ac417de2b4ac8ad3f83bc6fa63fb3b855a46c
appleshan <apple.shan@gmail.com>
2023-04-12 14:52:03 +0800

    nitrogen 更换所有屏幕的壁纸

---
+++ nitrogen/.config/nitrogen/nitrogen-change-all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#using the path WALLPAPERS will select a new random wallpaper
+WALLPAPERS=~/persist/pictures/wallpaper
+RANGE=$(ls $WALLPAPERS | wc -l)
+
+for i in 2 7 12; do
+  let "number = $RANDOM % $RANGE"
+  IMAGE=$(ls $WALLPAPERS | head -$number | tail -1 )
+  sed -i $i' c\file='"$WALLPAPERS/$IMAGE"'' ~/.config/nitrogen/bg-saved.cfg
+done
+
+nitrogen --restore
