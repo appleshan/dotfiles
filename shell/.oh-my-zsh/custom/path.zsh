b8c1e66ee519e4910363ba3c09538e9765859c6c
Alan.Shan <apple.shan@gmail.com>
2025-10-11 12:07:45 +0800

    系统升级后续，杂项维护

--- shell/.oh-my-zsh/custom/path.zsh
+++ shell/.oh-my-zsh/custom/path.zsh
@@ -23,6 +23,16 @@ source $HOME/.local/bin/z.sh
 export RIPGREP_CONFIG_PATH=$HOME/.config/ripgrep/.ripgreprc
 
 ###############
+# Perl5       #
+###############
+
+PATH="/home/alecshan/perl5/bin${PATH:+:${PATH}}"; export PATH;
+PERL5LIB="/home/alecshan/perl5/lib/perl5${PERL5LIB:+:${PERL5LIB}}"; export PERL5LIB;
+PERL_LOCAL_LIB_ROOT="/home/alecshan/perl5${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_ROOT}}"; export PERL_LOCAL_LIB_ROOT;
+PERL_MB_OPT="--install_base \"/home/alecshan/perl5\""; export PERL_MB_OPT;
+PERL_MM_OPT="INSTALL_BASE=/home/alecshan/perl5"; export PERL_MM_OPT;
+
+###############
 # Emacs Setup #
 ###############
 
