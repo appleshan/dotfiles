affc89ab74ef89581414079aa6f05bee594a496c
Alan.Shan <apple.shan@gmail.com>
2025-11-02 17:03:05 +0800

    feat: clipboard tool clipcat

--- shell/.oh-my-zsh/custom/functions.zsh
+++ shell/.oh-my-zsh/custom/functions.zsh
@@ -425,3 +425,12 @@ function proxy_off() {
           FTP_PROXY RSYNC_PROXY
     echo -e "Proxy environment variable removed."
 }
+
+# clipcat
+if type clipcat-menu >/dev/null 2>&1; then
+    alias clipedit=' clipcat-menu --finder=builtin edit'
+    alias clipdel=' clipcat-menu --finder=builtin remove'
+
+    bindkey -s '^\' "^Q clipcat-menu --finder=builtin insert ^J"
+    bindkey -s '^]' "^Q clipcat-menu --finder=builtin remove ^J"
+fi
