04652de1e35cb48f8457cf79ab72d7a99d03d251
Alan.Shan <apple.shan@gmail.com>
2026-01-10 10:35:41 +0800

    refactor: remove deprecated configuration files

--- unbound/unbound-install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-sudo pacman -S bind-utils
-sudo pacman -S unbound
-
-# sudo wget ftp://FTP.INTERNIC.NET/domain/named.cache -O /etc/unbound/root.hints
-# or
-sudo curl -o /etc/unbound/root.hints https://www.internic.net/domain/named.cache
