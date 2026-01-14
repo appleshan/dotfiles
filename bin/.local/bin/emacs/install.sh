39a34188205aaac5caeb12698b8da3b8f22e7881
Alan.Shan <apple.shan@gmail.com>
2026-01-14 14:09:41 +0800

    refactor: reorganize emacs scripts

---
+++ bin/.local/bin/emacs/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# -*- shell-scripts -*-
+
+# ~/projects/private/dotfiles/bin/.local/bin/emacs/ : emacs-client / emacs-terminal / emacs-shutdown / emacs-pkill
+
+# emacs-client
+ln -s ~/projects/private/dotfiles/bin/.local/bin/emacs/emacs-client ~/.local/bin/ec
+# emacs-terminal
+ln -s ~/projects/private/dotfiles/bin/.local/bin/emacs/emacs-terminal ~/.local/bin/et
+# emacs-shutdown
+ln -s ~/projects/private/dotfiles/bin/.local/bin/emacs/emacs-shutdown ~/.local/bin/es
+# emacs-pkill
+ln -s ~/projects/private/dotfiles/bin/.local/bin/emacs/emacs-pkill ~/.local/bin/ek
