7b5c9031078fd19fb56e9ea2604abe299b88f585
Alan.Shan <apple.shan@gmail.com>
2026-07-14 13:26:24 +0900

    refactor: 音量控制的执行脚本由替换 pactl 为 wpctl

--- i3wm/.config/metaesc/show_audio.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-audio=$(pactl list sinks | grep -e '^[[:space:]]Volume:' | tr -sc '0-9%' '\n' | grep % -m 1 2>/dev/null)
-mute=$(pactl list sinks | grep 'Mute: yes' 2>/dev/null)
-if [ -n "$mute" ];then
-    notify-send -t 500 "🎶 Mute"
-else
-    notify-send -t 500 "🎶 $audio"
-fi
