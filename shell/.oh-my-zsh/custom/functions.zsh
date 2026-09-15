bccab92d4e2d79e0087d25d7ee0a9dfe11e7bb47
Alan.Shan <apple.shan@gmail.com>
2026-09-15 03:56:52 -0700

    feat: Add 'y' to 'yazi' in zsh

--- shell/.oh-my-zsh/custom/functions.zsh
+++ shell/.oh-my-zsh/custom/functions.zsh
@@ -499,3 +499,11 @@ dockcp() {
 dockshi() {
   docker run -it $1 /bin/bash
 }
+
+function y() {
+    local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+    yazi "$@" --cwd-file="$tmp"
+    IFS= read -r -d '' cwd < "$tmp"
+    [ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+    rm -f -- "$tmp"
+}
